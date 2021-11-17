3bdd4b2910cd0b96352ae322d34ea441b12aac2c
aamrahms <aamrah.a@northeastern.edu>
2021-11-16 23:54:19 -0500

    Delivery man class modified

--- Assignment5/db4odemo/src/Business/DeliveryMan/DeliveryMan.java
+++ Assignment5/db4odemo/src/Business/DeliveryMan/DeliveryMan.java
@@ -5,10 +5,99 @@
  */
 package Business.DeliveryMan;
 
+import Business.Restaurant.Dish;
+import Business.Restaurant.Order;
+import java.util.ArrayList;
+
 /**
  *
  * @author harold
  */
 public class DeliveryMan {
+    private String name;
+    private String username;
+    private String password;
+    private String address;
+    private String phone;
+    private ArrayList <Order> deliveryOrders;
+
+    public DeliveryMan(String name, String username, String password, String address, String phone, ArrayList<Order> deliveryOrders) {
+        this.name = name;
+        this.username = username;
+        this.password = password;
+        this.address = address;
+        this.phone = phone;
+        this.deliveryOrders = new ArrayList<Order>();
+    }
+    //add order to deliveryOrders
+    
+    public void addOrder(String orderId,String restaurantName,String customerName, String deliverymanName,String price, String status, ArrayList<Dish> listOfDishes,String deliveryAddress){
+        Order order=new Order(orderId,restaurantName,customerName,deliverymanName,price,"New Order",listOfDishes,deliveryAddress);
+        if(deliveryOrders==null)
+        {
+            deliveryOrders=new ArrayList<Order>();
+        }
+        deliveryOrders.add(order);
+        
+    }
+    
+    //getters and setters
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public ArrayList<Order> getDeliveryOrders() {
+        if(deliveryOrders==null)
+        {
+            deliveryOrders=new ArrayList<Order>();
+        }
+        return deliveryOrders;
+    }
+
+    public void setDeliveryOrders(ArrayList<Order> deliveryOrders) {
+        this.deliveryOrders = deliveryOrders;
+    }
+    
+    @Override
+    public String toString(){
+        return name;
+    }
     
 }
